3376d8776e84de3692193a2516a948dc025a42f7
Patrick <shiawasearuji@gmail.com>
2024-11-17 20:44:45 +0800

    initial main and adding sub-operators

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from tambah import tambah
+from operator_vektor.penjumlahan_vektor import penjumlahan
+from operator_vektor.pengurangan_vektor import pengurangan
+from operator_vektor.dot_product import dot_product
+from operator_vektor.panjang_vektor import panjang_vektor
+from operator_vektor.vektor_unit import vektor_unit
+
 
 A = [2, 3]
 B = [4, 6]
@@ -13,21 +18,25 @@ def menu_vektor():
     print("3. Dot Product")
     print("4. Panjang Vektor")
     print("5. Vektor Unit")
-    pilihan = int(input("\nPilihan : "))
+    print("6. Keluar")
+    pilihan = int(input("\nmasukkan pilihan (1/2/3/4/5/6) : " .capitalize()))
+    
+    
+    
     return pilihan
 
-# def user_input():
+def user_input():
     
-#     pilihan = menu_vektor()
-#     if pilihan == 1:
-#         penjumlahan(vek_A, vek_B)
-#     elif pilihan == 2:
-#         pengurangan(vek_A, vek_B)
-#     elif pilihan == 3:
-#         dot_product(vek_A, vek_B)
-#     elif pilihan == 4:
-#         panjang_vektor(vek_A)
-#     elif pilihan == 5:
-#         vektor_unit(vek_A)
-#     else:
-#         print("Pilihan Tidak Tersedia.")
+    pilihan = menu_vektor()
+    if pilihan == 1:
+        penjumlahan(vek_A, vek_B)
+    elif pilihan == 2:
+        pengurangan(vek_A, vek_B)
+    elif pilihan == 3:
+        dot_product(vek_A, vek_B)
+    elif pilihan == 4:
+        panjang_vektor(vek_A)
+    elif pilihan == 5:
+        vektor_unit(vek_A)
+    else:
+        print("Pilihan Tidak Tersedia.")
---
+++ operator_vektor/penjumlahan_vektor.py
@@ -0,0 +1,3 @@
+# penjumlahan vektor
+
+import math
