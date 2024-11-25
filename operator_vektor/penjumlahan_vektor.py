6039d230f4346ca29403d6a6b53ad6887ed55309
Patrick <47711789+shiawasepat@users.noreply.github.com>
2024-11-25 15:16:10 +0800

    Delete operator_vektor directory
    ganti folder

--- operator_vektor/penjumlahan_vektor.py
+++
@@ -1,3 +0,0 @@
-# penjumlahan vektor
-
-import math
--- operator_vektor/vektor_unit.py
+++
@@ -1,9 +0,0 @@
-# unit vektor
-import math #module matematica
-
-def vektor_unit(v): #(v) =  vektor 
-    #panjang = magnitudo
-    panjang = math.sqrt((sum([i**2 for i in v])))
-    vektor_unit = [i/panjang for i in v] #normalisasi
-    vektor_unit = [round(i,2) for i in vektor_unit] #normalisasi
-    return vektor_unit
